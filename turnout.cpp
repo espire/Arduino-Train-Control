96396f249c3877d12de22fb15149320f73d40594
Eli Spiro <elispiro@gmail.com>
2011-06-20 23:13:07 -0400

    fix

---
+++ turnout.cpp
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include <AFMotor.h>
+#include <WProgram.h>
+#include "turnout.h"
+
+Turnout::Turnout(uint8_t motorNum) : AF_DCMotor(motorNum) {
+  setSpeed(255);
+}
+
+void Turnout::setStraight() {
+  if(state != STRAIGHT) {
+    run(FORWARD);
+    delay(20);
+    run(RELEASE);
+    state = STRAIGHT;
+  }
+}
+
+void Turnout::setTurned() {
+  if(state != TURNED) {
+    run(BACKWARD);
+    delay(20);
+    run(RELEASE);
+    state = TURNED;
+  }
+}
+
+SwitchState Turnout::throwSwitch() {
+  if(state != STRAIGHT) {
+    run(FORWARD);
+    delay(20);
+    run(RELEASE);
+    state = STRAIGHT;
+  }
+  else if(state != TURNED) {
+    run(BACKWARD);
+    delay(20);
+    run(RELEASE);
+    state = TURNED;
+  }
+  
+  return state;
+}
+
+SwitchState Turnout::getState() {
+  return state;
+}
